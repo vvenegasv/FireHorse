d827c8420f8da0f4dc41210cd19feb17f6766aa2
Vladimir Venegas Velásquez <vvenegasv@gmail.com>
2017-03-02 19:53:21 -0300

    Se modifica para crear packete nuget

--- src/FireHorse/FireHorse/Properties/AssemblyInfo.cs
+++ src/FireHorse/FireHorse/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FireHorseManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Complemento que facilita realizar web scraper. Implementa el patrón productor/consumidor para crear workers que obtienen el HTML desde los diferentes web servers, así como limitar la cantidad de ellos para no saturar al servidor ya mencionado")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FireHorseManager")]
+[assembly: AssemblyCompany("Infodinamica Limitada")]
+[assembly: AssemblyProduct("FireHorse")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
